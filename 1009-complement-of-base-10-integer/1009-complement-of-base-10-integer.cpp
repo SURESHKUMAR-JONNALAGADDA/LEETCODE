817e4a73b4154f8f52ea27fd88eb799588c994c3
SURESH <109402264+SURESHKUMAR-JONNALAGADDA@users.noreply.github.com>
2026-03-11 20:58:51 +0530

    Time: 0 ms (100%), Space: 8.1 MB (4.02%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0)
+        return 1;
+        int x = log2(n);
+        x = pow(2,x+1)-1;
+        return x-n;
+    }
+};
